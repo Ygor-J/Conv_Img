91b6e8e6e94699f0ee2595befd58687115fd3655
YgorJ <ygor-skat@hotmail.com>
2021-10-12 23:07:52 -0300

    Add support for kernel of any size

--- main.c
+++ main.c
@@ -61,14 +61,15 @@ void print_matrix(FILE *newFileP, int **matrix, int width, int height){
 /**
  * Applies convolution using kernel
  * */
-int **apply_convolution(int **matrix, Dimension D, int kernel[][3]){
+int **apply_convolution(int **matrix, Dimension D, float **kernel, int kernel_size){
     int **conv_matrix = allocate_matrix(D.width, D.height);
-    for(int i=1; i<D.height-1; i++){
-        for(int j=3; j<3*(D.width-3); j++){
-            int value = 0;
-            for(int k=0; k<3; k++){
-                for(int l=0; l<3; l++){
-                    value += kernel[k][l] * matrix[i-1+k][j-3+3*k];
+    int num_pixels_skip = (kernel_size-1)/2;
+    for(int i=num_pixels_skip; i<D.height-num_pixels_skip; i++){
+        for(int j=3*num_pixels_skip; j<3*(D.width-num_pixels_skip); j++){
+            float value = 0;
+            for(int k=0; k<kernel_size; k++){
+                for(int l=0; l<kernel_size; l++){
+                    value += kernel[k][l] * matrix[i-num_pixels_skip+k][j-3*num_pixels_skip+3*k];
                 }
             }
             if(value > 255){
@@ -76,10 +77,43 @@ int **apply_convolution(int **matrix, Dimension D, int kernel[][3]){
             }else if(value < 0){
                 value = 0;
             }
-            conv_matrix[i][j] = value;
+            conv_matrix[i][j] = (int) value;
         }
     }
     return conv_matrix;
+}
+
+/**
+ * Create new file
+ * */
+void write_file(int **conv_matrix, char *type, int limit, Dimension *D){
+    FILE *newFileP = fopen("file_processed.ppm", "w+");
+    fprintf(newFileP, "%s\n", type);
+    fprintf(newFileP, "%d %d\n", D->width, D->height);
+    fprintf(newFileP, "%d\n", limit);
+    print_matrix(newFileP, conv_matrix, D->width, D->height);
+    fclose(newFileP);
+}
+
+/**
+ * Reads square matrix/kernel from stdin
+ * */
+float **read_kernel(int *kernel_size){
+    printf("Enter size of kernel: ");
+    scanf("%d", kernel_size);
+    if(*kernel_size % 2 == 0){
+        printf("Only kernels with odd size are allowed.\n");
+        exit(0);
+    }
+    float **kernel = malloc(*kernel_size * sizeof(float *));
+    printf("Please, enter kernel: \n");
+    for(int i=0; i<*kernel_size; i++){
+        kernel[i] = calloc(*kernel_size, sizeof(float));
+        for(int j=0; j<*kernel_size; j++){
+            scanf("%f", &kernel[i][j]);
+        }
+    }
+    return kernel;
 }
 
 int main(){
@@ -92,19 +126,11 @@ int main(){
     scanf("%s", path);
 
     int **matrix = read_file(path, type, &limit, D);
-    int kernel[3][3] = {
-        {2, -1, 0},
-        {-1, 1, 1},
-        {0, 1, 2}
-    };
+    int kernel_size;
+    float **kernel = read_kernel(&kernel_size);
 
-    int **conv_matrix = apply_convolution(matrix, *D, kernel);
+    int **conv_matrix = apply_convolution(matrix, *D, kernel, kernel_size);
 
-    FILE *newFileP = fopen("file_processed.ppm", "w+");
-    fprintf(newFileP, "%s\n", type);
-    fprintf(newFileP, "%d %d\n", D->width, D->height);
-    fprintf(newFileP, "%d\n", limit);
-    print_matrix(newFileP, conv_matrix, D->width, D->height);
-    fclose(newFileP);
+    write_file(conv_matrix, type, limit, D);
     return 0;
 }
